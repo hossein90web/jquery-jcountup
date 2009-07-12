b3de78c66ac1e32e936ba413d03f6ad5e37b80a0
Anton Lindqvist <anton@qvister.se>
2009-07-12 17:25:31 +0200

    Code clean up.

--- jquery.jcountup.js
+++ jquery.jcountup.js
@@ -1,38 +1,41 @@
 /**
  * jQuery jCountUp Plugin
  *
- * version: 0.1
+ * version: 1.0
  * author: Anton Lindqvist <anton@qvister.se>
  * link: http://qvister.se
+ *
+ * Examples and documentation: https://github.com/mptre/jquery-jcountup/
  */
+
 (function($) {
-$.fn.jCountUp = function(options) {
-  var i, c, tmp, defaults, update;
-  i = 0;
-  c = 0;
-  defaults = { val: 10, speed: 50, up: true, callback: function() {} }; // default arguments.
-  options = $.extend(defaults, options); // merge defaults with submitted arguments.
-  update = function(el, c) { // update the value of the element.
-    tmp = parseInt($(el).text(), 10);
-    
-    if (options.up) { // count up.
-      tmp += 1;
-    } else { // count down.
-      tmp -= 1;
-    }
-    
-    $(el).text(tmp);
-    
-    if (options.val === c) {
-      options.callback.call(el); // execute callback function.
-    }
-  };
-  
-  for (; i < options.val; i += 1) {
-    $(this).fadeTo(options.speed, 1, function() { // ugly sleep hack since setTimeout() isn't working correctly here.
-      c += 1;
-      update(this, c);
-    });
-  }
-};
+    $.fn.jCountUp = function(options) {
+        var c, defaults, i, tmp, update;
+        i = 0;
+        c = 0;
+        defaults = { val: 10, speed: 50, up: true, callback: function() {} }; // default arguments.
+        options = $.extend(defaults, options); // merge defaults with submitted arguments.
+        update = function(el, c) { // update the value of the element.
+            tmp = parseInt($(el).text(), 10);
+
+            if (options.up) { // count up.
+                tmp += 1;
+            } else { // count down.
+                tmp -= 1;
+            }
+
+            $(el).text(tmp);
+
+            if (options.val === c) {
+                options.callback.call(el); // execute callback function.
+            }
+        };
+
+        for (; i < options.val; i += 1) {
+            $(this).fadeTo(options.speed, 1, function() { // ugly sleep hack since setTimeout() isn't working correctly here.
+                c += 1;
+                update(this, c);
+            });
+        }
+    };
 })(jQuery);
